d2b8efcb2a3a43d1f8647cfe10ed68b334eaa4f6
kab33raa <kabeerbhowmik@gmail.com>
2026-01-19 14:24:50 +0530

    Add KABEER class with main method for introduction

---
+++ 1.java
@@ -0,0 +1,13 @@
+package com.mycompany.kabeer;
+
+public class KABEER {
+
+    public static void main(String[] args) {
+        System.out.println("************************************************\n               Introduction \n**************************************************");
+        System.out.println("NAME: KABEER BHOWMIK");
+        System.out.println("CLASS: BCA AI&ML");
+        System.out.println("SECT------------------------------------------------------------------------------------\");ION: 2B");
+        System.out.println("SUBJECT: JAVA");
+         System.out.println("---------------");
+    }
+}
